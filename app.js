4b513e5668e0c024a9bad090991c424aca910d0d
M.Vamsi Krishna <79745506+Vamsimaram@users.noreply.github.com>
2024-07-03 16:59:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ const listingRouter=require("./routes/listing.js")
 const reviewRouter=require("./routes/review.js");
 const userRouter=require("./routes/user.js");
 
-//const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
-const dbUrl=process.env.ATLASDB_URL;
+//const dbUrl=process.env.ATLASDB_URL;
 
 main()
     .then(()=>{
@@ -39,7 +39,7 @@ main()
     });
 
 async function main(){
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(MONGO_URL);  //dbUrl
 }
 
 app.set("view engine","ejs");
@@ -50,20 +50,20 @@ app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")))
 
 //session data
-const store=MongoStore.create({
-    mongoUrl:dbUrl,
-    crypto:{
-        secret:process.env.SECRET,
-    },
-    touchAfter:24*3600,
-})
+// const store=MongoStore.create({
+//     mongoUrl:dbUrl,
+//     crypto:{
+//         secret:process.env.SECRET,
+//     },
+//     touchAfter:24*3600,
+// })
 
-store.on("error",()=>{
-    console.log("ERROR IN MONGO SESSION STORE",err);
-});
+// store.on("error",()=>{
+//     console.log("ERROR IN MONGO SESSION STORE",err);
+// });
 
 const sessionOptions={
-    store,
+    //store,
     secret:process.env.SECRET,
     resave:false,
     saveUninitialized:true,
